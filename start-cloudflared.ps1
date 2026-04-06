7461bf36543add81f400e98f13571c7a5f1e0097
PotatoBin <spmtaebin@cau.ac.kr>
2026-04-06 11:08:46 +0900

    Add Docker and cloudflared setup

---
+++ start-cloudflared.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = 'Stop'
+
+$root = Split-Path -Parent $MyInvocation.MyCommand.Path
+$configPath = Join-Path $root '.cloudflared\config.yml'
+$stdoutPath = Join-Path $root 'cloudflared.stdout.log'
+$stderrPath = Join-Path $root 'cloudflared.stderr.log'
+
+Start-Process `
+  -FilePath 'cloudflared' `
+  -ArgumentList @('tunnel', '--config', $configPath, 'run', 'terarium-tutorial') `
+  -WorkingDirectory $root `
+  -RedirectStandardOutput $stdoutPath `
+  -RedirectStandardError $stderrPath
